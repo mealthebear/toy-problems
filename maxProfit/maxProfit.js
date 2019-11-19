206456604a6c239f1f31bc8dcee58822c76222a8
Dennis <mealthebear@gmail.com>
2019-11-18 17:46:40 -0800

    Implemented pseudocode for maxProfit

--- maxProfit/maxProfit.js
+++ maxProfit/maxProfit.js
@@ -7,6 +7,32 @@
 // Note: You may not engage in multiple transactions at the same time 
 // (i.e., you must sell the stock before you buy again).
 
+// Example 1: 
+// Input: [7,1,5,3,6,4]
+// Output: 7
+
+// Example 2:
+// Input: [1,2,3,4,5]
+// Output: 4
+
+// Example 3:
+// Input: [7,6,4,3,1]
+// Output: 0
+
+// Input: An array of integers (stock prices)
+// Output: An integer representing max profit possible
+// Constraints: None
+// Edge Cases: If input array only has 1 number
+
+/* ** Strategy ** 
+First can check if the input array's numbers are all sorted
+descendingly. Then we know that the max profit is 0. Then, you start iterating
+through the array. On each day, you check if the next day is lesser or greater. If the
+next day's price is greater, then you buy on the current day, and sell on the next day.
+If the next day's price is lesser or equal, then don't buy and wait for the next day. Keep going
+until you reach the end of the array. Everytime you buy and sell, add it an output variable
+*/
+
 let maxProfit = (prices) => {
-    
+
 }
