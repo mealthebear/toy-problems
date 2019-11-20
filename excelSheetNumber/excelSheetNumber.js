427b75b6debdc9f216c2527c99d26069f42d5937
Dennis <mealthebear@gmail.com>
2019-11-19 20:30:56 -0800

    Created pseudocode for excelSheetNumber

--- excelSheetNumber/excelSheetNumber.js
+++ excelSheetNumber/excelSheetNumber.js
@@ -26,6 +26,21 @@
 // Input: "ZY"
 // Output: 701
 
+// Input: A string of letters
+// Output: An integer (column number)
+// Constraints: None
+// Edge Cases: If input string is empty
+
+/* ** Strategy ** 
+First we can make an object which has the keys as letters and the
+values as their corresponding value. Then we can initialize a counter
+variable that will keep track of each letter's value. Next, we can initialize
+an index variable set to the length of the input string and we decrement everytime
+we go to the next character in the string. Then, we can iterate through the 
+input string and at each letter, we take the value of the current letter and
+add 26 times the number of succeeding letters remaining.
+*/
+
 let titleToNumber = (s) => {
     
 };
