3b0a597df502a07a895b650fd366b5491bbdbc39
Dennis <mealthebear@gmail.com>
2019-11-25 17:06:36 -0800

    (problem) toeplitzMatrix

---
+++ toeplitzMatrix/toeplitzMatrix.js
@@ -0,0 +1,32 @@
+// A matrix is Toeplitz if every diagonal from top-left to bottom-right has the same element.
+
+// Now given an M x N matrix, return True if and only if the matrix is Toeplitz.
+
+// Example 1:
+
+// Input:
+// matrix = [
+//   [1,2,3,4],
+//   [5,1,2,3],
+//   [9,5,1,2]
+// ]
+// Output: True
+
+// Example 2:
+
+// Input:
+// matrix = [
+//   [1,2],
+//   [2,2]
+// ]
+// Output: False
+
+// Note:
+
+// matrix will be a 2D array of integers.
+// matrix will have a number of rows and columns in range [1, 20].
+// matrix[i][j] will be integers in range [0, 99].
+
+let isToeplitzMatrix = (matrix) => {
+    
+};
